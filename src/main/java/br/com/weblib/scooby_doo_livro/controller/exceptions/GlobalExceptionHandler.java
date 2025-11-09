63a58b3bb446b8c20106e6ef2e7b42240cfb549a
Kaio <kakacnmendes@gmail.com>
2025-11-09 12:33:03 -0300

    adiciona o tratador de excecoes

---
+++ src/main/java/br/com/weblib/scooby_doo_livro/controller/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package br.com.weblib.scooby_doo_livro.controller.exceptions;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+}
