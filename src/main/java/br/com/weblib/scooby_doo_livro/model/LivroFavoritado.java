5233cd5294816df98900ec7711237e12293626a4
Kaio <kakacnmendes@gmail.com>
2025-11-09 12:17:30 -0300

    adiciona a model livro favoritado

--- src/main/java/br/com/weblib/scooby_doo_livro/model/LivroFavoritado.java
+++ src/main/java/br/com/weblib/scooby_doo_livro/model/LivroFavoritado.java
@@ -1,4 +1,10 @@
 package br.com.weblib.scooby_doo_livro.model;
 
+import java.util.Date;
+
 public class LivroFavoritado {
+    private Long id;
+    private Long idLivro;
+    private Long idUsuario;
+    private Date dataAdicao;
 }
