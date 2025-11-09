f469d7c8dfa4f45231e28a66ff7603d1ac445891
Kaio <kakacnmendes@gmail.com>
2025-11-09 12:32:11 -0300

    adiciona a livro controller

---
+++ src/main/java/br/com/weblib/scooby_doo_livro/controller/LivroController.java
@@ -0,0 +1,9 @@
+package br.com.weblib.scooby_doo_livro.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/livro")
+public class LivroController {
+}
