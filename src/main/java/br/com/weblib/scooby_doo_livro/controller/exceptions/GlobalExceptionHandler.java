6b10122deccd0a0258ee1895d911925a59fffc4c
Kaio <kakacnmendes@gmail.com>
2025-11-09 12:35:56 -0300

    adiciona o tratamento de excecoes para recursos nao encontrados

--- src/main/java/br/com/weblib/scooby_doo_livro/controller/exceptions/GlobalExceptionHandler.java
+++ src/main/java/br/com/weblib/scooby_doo_livro/controller/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,28 @@
 package br.com.weblib.scooby_doo_livro.controller.exceptions;
 
+import br.com.weblib.scooby_doo_livro.domain.model.ApiErrorResponse;
+import br.com.weblib.scooby_doo_livro.domain.model.exceptions.RecursoNaoEncontradoException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RecursoNaoEncontradoException.class)
+    public ResponseEntity<ApiErrorResponse> handleRecursoNaoEncontrado(
+            RecursoNaoEncontradoException e,
+            HttpServletRequest request
+    ) {
+        ApiErrorResponse errorResponse = new ApiErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                "Dados não foram encontrados.",
+                e.getMessage(),
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
 }
