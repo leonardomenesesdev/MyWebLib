3c3680190003cd9a2217bd069040316fd40bafb4
Kaio <kakacnmendes@gmail.com>
2025-11-09 12:17:08 -0300

    adiciona a model comentario

--- src/main/java/br/com/weblib/scooby_doo_livro/model/Comentario.java
+++ src/main/java/br/com/weblib/scooby_doo_livro/model/Comentario.java
@@ -1,4 +1,19 @@
 package br.com.weblib.scooby_doo_livro.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Comentario {
+    private Long id;
+    private Long idUsuario;
+    private Long idLivro;
+    private String conteudo;
+    private Date data;
+
 }
