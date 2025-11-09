2a2908a59ff6e4a1be071d40cb4f67ed3585e162
Kaio <kakacnmendes@gmail.com>
2025-11-09 12:18:07 -0300

    adiciona a model usuario

--- src/main/java/br/com/weblib/scooby_doo_livro/model/Usuario.java
+++ src/main/java/br/com/weblib/scooby_doo_livro/model/Usuario.java
@@ -1,4 +1,15 @@
 package br.com.weblib.scooby_doo_livro.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Usuario {
+    private Long id;
+    private String nome;
+    private String email;
+    private String hashSenha;
 }
