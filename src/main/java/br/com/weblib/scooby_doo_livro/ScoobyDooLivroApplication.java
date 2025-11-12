1f6e540182dbf05207563d1dd786ad2e0c4e7c9e
Leonardo <leonardomenesesc@edu.unifor.br>
2025-11-12 16:47:24 -0300

    fix: alunorepository

--- src/main/java/br/com/weblib/scooby_doo_livro/ScoobyDooLivroApplication.java
+++ src/main/java/br/com/weblib/scooby_doo_livro/ScoobyDooLivroApplication.java
@@ -1,6 +1,5 @@
 package br.com.weblib.scooby_doo_livro;
 
-import br.com.weblib.scooby_doo_livro.AlunoRepository.UsuarioRepository;
 import br.com.weblib.scooby_doo_livro.domain.model.Usuario;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,15 +12,4 @@ public class ScoobyDooLivroApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ScoobyDooLivroApplication.class, args);
 	}
-    @Bean
-    CommandLineRunner init(UsuarioRepository usuarioRepository) {
-        return args -> {
-            Usuario u = new Usuario();
-            u.setNome("Leonardo");
-            u.setEmail("leo@teste.com");
-            u.setHashSenha("1234");
-            usuarioRepository.save(u);
-        };
-    }
-
 }
