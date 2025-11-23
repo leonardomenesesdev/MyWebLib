b88f0dd842bac227cf66b2f0f0ccbf409d63388b
Kaio <kakacnmendes@gmail.com>
2025-11-23 09:41:21 -0300

    altera a resposta para avaliacao de livros

--- src/main/java/br/com/weblib/scooby_doo_livro/controller/AvaliacaoController.java
+++ src/main/java/br/com/weblib/scooby_doo_livro/controller/AvaliacaoController.java
@@ -1,6 +1,7 @@
 package br.com.weblib.scooby_doo_livro.controller;
 
 import br.com.weblib.scooby_doo_livro.domain.model.Avaliacao.Avaliacao;
+import br.com.weblib.scooby_doo_livro.domain.model.Avaliacao.AvaliacaoResponseDTO;
 import br.com.weblib.scooby_doo_livro.domain.service.AvaliacaoService;
 import br.com.weblib.scooby_doo_livro.domain.service.LivroService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +19,12 @@ public class AvaliacaoController {
     private LivroService livroService;
 
     @PostMapping("/avaliar/{livroId}/{nota}/{usuarioId}")
-    public ResponseEntity<Avaliacao> avaliar(@PathVariable Long livroId,
-                                             @PathVariable Integer nota,
-                                             @PathVariable Long usuarioId) {
+    public ResponseEntity<AvaliacaoResponseDTO> avaliar(@PathVariable Long livroId,
+                                                        @PathVariable Integer nota,
+                                                        @PathVariable Long usuarioId) {
         Avaliacao avaliacao = avaliacaoService.avaliarLivro(livroId, usuarioId,
                 nota);
-        return ResponseEntity.ok(avaliacao);
+        return ResponseEntity.ok(new AvaliacaoResponseDTO(avaliacao.getId(),
+                livroId, avaliacao.getUsuario().getEmail(), avaliacao.getNota()));
     }
 }
