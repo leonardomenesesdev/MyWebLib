b6677d214c088dabd20fe5b0e331fa7ef5eecb75
Kaio <kakacnmendes@gmail.com>
2025-11-23 09:40:59 -0300

    altera o objeto de respostas para as avaliacoes de livros

--- src/main/java/br/com/weblib/scooby_doo_livro/domain/model/Avaliacao/AvaliacaoResponseDTO.java
+++ src/main/java/br/com/weblib/scooby_doo_livro/domain/model/Avaliacao/AvaliacaoResponseDTO.java
@@ -1,5 +1,5 @@
 package br.com.weblib.scooby_doo_livro.domain.model.Avaliacao;
 
 public record AvaliacaoResponseDTO(Long avaliacaoId, Long livroId,
-                                   String emailUsuario) {
+                                   String emailUsuario, Integer nota) {
 }
